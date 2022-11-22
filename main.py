9ceef07b311a90f363ba7c8961025e09485f5db6
Russell Pacheco <russ1487@gmail.com>
2022-11-22 10:36:42 +0900

    Redid the logic for the entire program removing having most of the logic for folder creation, process forking, and logging from watchdog. Added in support for rabbitmq with celery to allow for separation of log files between program-specific and aria2c specific log files. Cleaned up on_complete template file, and on_kill. Also removed the creation of the running_ps json file as it became redundant. Lastly, in on_complete, checks for if other processes related to it's parent folder are running before attempting to run on_kill to allow for mutiple downloads related to a single watch folder parent directory. Still need to test on parallel downloads with different and same watch folder parents.

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-import os, subprocess, json, argparse, shutil, stat
+import os, subprocess, json, argparse, shutil, stat, datetime, logging
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from torrent_process import handle_torrent_download
 
 
 parser = argparse.ArgumentParser(prog="Nas Torrent Auto Downloader", description="Automatically watches specified download folders for torrent files, and if found, downloads torrent.")
 parser.add_argument("-w", "--watch-path", required=True, help="Set the folder to watch")
 parser.add_argument("-d", "--download-folder", required=True, help="Set the download folder")
+parser.add_argument("-l", "--log-file", required=True, help="Specify the log file path.")
+parser.add_argument("--log-level", required=False, help="DEBUG, INFO, WARNING, ERROR, CRITICAL", default="INFO")
+
 args = parser.parse_args()
+
+log_file = args.log_file
+log_level = args.log_level
+
+numeric_level = getattr(logging, log_level.upper(), None)
+if not isinstance(numeric_level, int):
+    raise ValueError('Invalid log level: %s' % log_level)
+
+logging.basicConfig(filename=log_file, encoding="utf-8", level=log_level, format="[%(asctime)s] %(message)s")
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 watch_path = os.path.normpath(args.watch_path)
@@ -21,71 +34,23 @@ class Callback(FileSystemEventHandler):
         watch_folder_parent = os.path.split(watch_path)[1]
         torrent_file = os.path.basename(event.key[1])
         torrent_file_abspath = event.key[1]
+        new_folder_path = os.path.join(download_folder, parent)
 
         if torrent_file.endswith('.torrent'):
             if parent != watch_folder_parent and parent_parent == watch_folder_parent:
-                print(f"""
-New file detected!
-    - Filename: {torrent_file}
-    - Filepath: {torrent_file_abspath}
-    - Parent_dir: {parent}       
-                """)
-                new_folder_path = os.path.join(download_folder, parent)
-                self._create_folder(new_folder_path=new_folder_path)
-                download_ps = self._run_torrent_downloader(torrent_file_abspath=torrent_file_abspath, new_folder_path=new_folder_path, watch_folder_parent_path=parent_dir)
-                #download_ps = self._run_test_process(dp_uuid=dp_uuid)
-                self._copy_on_complete_files(parent_dir=parent_dir, pid=download_ps.pid)
-                self._update_json(download_pid=download_ps.pid, parent=parent)
-       
-    def _create_folder(self, new_folder_path: str):
-        try:
-            os.mkdir(new_folder_path)
-            print(f"File folder is created at {new_folder_path}")
-        except FileExistsError:
-            pass
-        return new_folder_path
-
-    def _copy_on_complete_files(self, parent_dir, pid):
-        data_file = os.path.join(parent_dir, "data")
-        with open(data_file, "w") as file:
-            data = {
-                "pid": pid,
-                "project_root": PROJECT_ROOT
-            }
-            json.dump(data, file)       
-        on_complete_file = os.path.join(PROJECT_ROOT, "on_complete.py")
-        copied_complete_file = os.path.join(parent_dir, "on_complete.py")
-        shutil.copyfile(on_complete_file, copied_complete_file)
-        os.chmod(copied_complete_file, stat.S_IRWXU)
-
-    def _run_test_process(self):
-        download_ps = subprocess.Popen(["python", "ongoing_process.py"])
-        print(f"Test process is being run at PID: {download_ps.pid}")
-        return download_ps
-
-    def _run_torrent_downloader(self, torrent_file_abspath: str, new_folder_path: str, watch_folder_parent_path: str):
-        #devnull = open(os.devnull, "wb")
-        on_complete_file_abspath = os.path.join(watch_folder_parent_path, "on_complete.py")
-        download_ps = subprocess.Popen(["aria2c", "-T", torrent_file_abspath, "-d", new_folder_path, "--file-allocation=falloc", "-V", "true", f'--on-bt-download-complete={on_complete_file_abspath}', "--show-console-readout=false"], start_new_session=True)
-        print(f"Aria is being run at PID: {download_ps.pid}")
-        return download_ps
-    
-    def _update_json(self, download_pid: str, parent: str):
-        with open("running_ps.json", "w+") as file:
-            data = None
-            try:
-                data = json.load(file)
-            except json.JSONDecodeError:
-                data = {} 
-
-            data["watch_folder"] = watch_path
-
-            if "running_ps" not in data:
-                data["running_ps"] = f"{download_pid}:{parent}"
-            else:
-                data["running_ps"] = f"{data['running_ps']};{download_pid}:{parent}"
-
-            file.write(json.dumps(data))
+                logging.info(f"New file detected:: Filename: {torrent_file} Filepath: {torrent_file_abspath} Parent_dir: {parent}")
+                print(f"[{datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')}] New file detected:: Filename: {torrent_file} Filepath: {torrent_file_abspath} Parent_dir: {parent}")
+                torrent_task_details = {
+                    "new_folder_path": new_folder_path,
+                    "torrent_file_abspath": torrent_file_abspath,
+                    "watch_folder_parent_path": parent_dir,
+                    "parent_dir": parent_dir,
+                    "parent": parent,
+                    "watch_path": watch_path,
+                    "log_file": log_file,
+                    "log_level": log_level
+                }
+                handle_torrent_download.delay(torrent_task_details)
 
 observer = Observer()
 callback = Callback()
@@ -93,7 +58,8 @@ callback = Callback()
 path = watch_path
 
 observer.schedule(event_handler=callback, path=path, recursive=True)
-print("Watch Dog is running")
+logging.info(f"Watch Dog is running")
+print(f"[{datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')}] Watch Dog is running")
 observer.start()
 
 
--- on_kill.py
+++ on_kill.py
@@ -1,26 +1,24 @@
-import shutil, os, json, sys
+import shutil, os, json, sys, datetime, logging
 
 
 PID_to_kill = sys.argv[1]
-
+log_file = sys.argv[2]
+parent_dir = sys.argv[3]
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+logging.basicConfig(filename=log_file, encoding="utf-8", format="[%(asctime)s] %(message)s", level="INFO", force=True)
+
 json_file = os.path.join(PROJECT_ROOT, "running_ps.json")
 with open(json_file, "r") as file:
     data = json.load(file)
 
 ID_list = data["running_ps"].split(";")
 
-for ID_pair in ID_list:
-    PID_dirname = ID_pair.split(":")
-    if PID_to_kill in PID_dirname:
-        try: 
-            shutil.rmtree(os.path.join(data["watch_folder"], PID_dirname[1]))
-            os.remove(os.path.join(PROJECT_ROOT, f"{PID_to_kill}.log"))
-        except FileNotFoundError:
-            pass
-        to_delete = ID_pair
-ID_list.remove(to_delete)
-data["running_ps"] = ";".join(ID_list)
-
-with open(json_file, "w") as file:
-    file.write(json.dumps(data))
+try: 
+    folder_to_delete = parent_dir
+    shutil.rmtree(folder_to_delete)
+    logging.info(f"Removing {folder_to_delete}")
+except FileNotFoundError:
+    logging.error(f"Folder Not Found Error Folder: {folder_to_delete}")
+except Exception as e:
+    logging.error(f"Folder Delete Error Folder: {folder_to_delete} Error: {e}")
